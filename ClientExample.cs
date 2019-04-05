e99876f5d1f6a6f9502aff519254ef46770770db
Daniel Wood <daniel.wood4@extendhealth.com>
2019-04-05 17:30:17 -0600

    Beginning to parse json messages

--- ClientExample.cs
+++ ClientExample.cs
@@ -7,6 +7,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Ionic.Zlib;
 using Newtonsoft.Json;
+using EDRP.Models;
+using EDRP.Journal.Models;
+using EDRP.Outfitting.Models;
+using EDRP.Commodity.Models;
+using EDRP.Shipyard.Models;
 
 namespace EDRP.client
 {
@@ -26,10 +31,29 @@ namespace EDRP.client
                     var bytes = client.ReceiveFrameBytes();
                     var uncompressed = ZlibStream.UncompressBuffer(bytes);
 
-                    var result = utf8.GetString(uncompressed);
-                    // var obj = JsonConvert.DeserializeObject(result);
+                    var result = utf8.GetString(uncompressed).Remove(2,1);
+                    var obj = JsonConvert.DeserializeObject<BaseDataModel<JournalMessage>>(result);
+                    Console.WriteLine(obj.Message.StarSystem);
+                    // var obj = JsonConvert.DeserializeObject<BasicJsonModel>(result);
+                    // if (obj.schemaRef == "https://eddn.edcd.io/schemas/journal/1")
+                    // {
+                    //     var msg = JsonConvert.DeserializeObject<JournalMessage>(obj.message);
+                    // }
+                    // else if (obj.schemaRef == "https://eddn.edcd.io/schemas/outfitting/2")
+                    // {
+                    //     var msg = JsonConvert.DeserializeObject<OutfittingMessage>(obj.message);
+                    // }
+                    // else if (obj.schemaRef == "https://eddn.edcd.io/schemas/shipyard/2")
+                    // {
+                    //     var msg = JsonConvert.DeserializeObject<ShipyardMessage>(obj.message);
+                    // }
+                    // else if (obj.schemaRef == "https://eddn.edcd.io/schemas/commodity/3")
+                    // {
+                    //     var message = JsonConvert.DeserializeObject<CommodityMessage>(obj.message);
+                    //     Console.WriteLine(message.systemName);
+                    // }
 
-                    Console.WriteLine(result);
+                    // Console.WriteLine(result);
                 }
             }
         }
---
+++ Models/BasicJsonModel.cs
@@ -0,0 +1,9 @@
+namespace EDRP.Models
+{
+    public class BasicJsonModel
+    {
+        public string schemaRef { get; set; }
+        public Header header { get; set; }
+        public string message { get; set; }
+    }
+}
